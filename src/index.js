be1a2442823591dca3fa65f3720aec4945fc04d6
Weslley <weslley@pop-os.localdomain>
2022-08-19 18:03:19 -0300

    feat: implementa props

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Pedido from './pedido';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -8,9 +9,46 @@ const App = ()=>{
     return (
         //componente principal
     <div className='container border rounded mt-2'>
+
         <div className='row border-bottom m-2'>
             <h1 className='display-5 text-center'>Seus pedidos</h1>
         </div>
+
+        <div className="row">
+            <div className="col-sm-8 col-md-6 m-2">
+                {/*Entrada do primeiro pedido*/}
+                <Pedido 
+                data='19/08/2022' 
+                icone="fa-solid fa-hdd fa-2x" 
+                titulo="Kingston"
+                descricao="SSD Kingston A400 - SATA"/>
+            </div>
+        </div>
+
+        {/*linha para o segundo pedido */}
+        <div className="row">
+            <div className="col-sm-8 col-md-6 m-2">
+                {/*Entrada do segundo pedido*/}
+                <Pedido 
+                data='20/08/2022' 
+                icone="fa-solid fa-book fa-2x" 
+                titulo="Livro"
+                descricao="Concrete Mathematics - Donald Knuth"/>
+            </div>
+        </div>
+
+        {/*linha para o terceiro pedido */}
+        <div className="row">
+            <div className="col-sm-8 col-md-6 m-2">
+                {/*Entrada do terceiro pedido*/}
+                <Pedido 
+                data='21/08/2022' 
+                icone="fa-solid fa-laptop fa-2x" 
+                titulo="Notebook"
+                descricao="Notebook Dell- 8Gb - i5"/>      
+            </div>
+        </div>
+
     </div>
 
     )
---
+++ src/pedido.js
@@ -0,0 +1,19 @@
+
+const Pedido = (props)=>{
+    return(
+    <div className="card">
+        <div className="card-header text muted">{props.data}</div>
+        <div className="card-body d-flex">
+            <div className="d-flex align-items-center">
+                <i className={props.icone}></i>
+            </div>
+            <div className="flex-grow-1 ms-2 border">
+                <h4 className="text-center">{props.titulo}</h4>
+                <p className="text-center">{props.descricao}</p>
+            </div>
+        </div>
+    </div>
+    )
+};
+
+export default Pedido
